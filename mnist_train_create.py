25c90201caca5c3eea124042efcc0b0348123d3d
Ouadjou Tarik <tarik.ouadjou@student-cs.fr>
2024-11-17 16:21:21 +0100

    Ajout dataset mnist

---
+++ mnist_train_create.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape((train_images.shape[0], 28 * 28))
+df = pd.DataFrame(train_images)
+df.insert(0, 'label', train_labels)
+df.to_csv('mnist_train_test.csv', index=False, header=False)
+
+print("Fichier 'mnist_train.csv' créé avec succès!")
